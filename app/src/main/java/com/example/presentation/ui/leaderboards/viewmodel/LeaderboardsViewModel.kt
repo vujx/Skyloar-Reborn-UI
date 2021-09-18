8f68f3a1b2ff9cfc24c8581b61abaa2f63051a9c
tv <tomislav.vujevic@undabot.com>
2021-09-18 21:41:01 +0200

    Refactor viewmodel for maps-months-difficulties

--- app/src/main/java/com/example/presentation/ui/leaderboards/viewmodel/LeaderboardsViewModel.kt
+++ app/src/main/java/com/example/presentation/ui/leaderboards/viewmodel/LeaderboardsViewModel.kt
@@ -3,25 +3,36 @@ package com.example.presentation.ui.leaderboards.viewmodel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.data.usecase.leaderboards.LeaderBoardsUseCase
-import kotlinx.coroutines.CoroutineExceptionHandler
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.cancel
+import com.example.domain.usecase.leaderboards.GetMaps
+import com.example.domain.usecase.leaderboards.GetRanges
 import kotlinx.coroutines.launch
+import com.example.util.Result
 
-class LeaderboardsViewModel(private val useCaseLeaderboards: LeaderBoardsUseCase) : ViewModel() {
+class LeaderboardsViewModel(private val getMapsUseCase: GetMaps, private val getRanges: GetRanges) : ViewModel() {
 
   val listOfRanges = MutableLiveData<Map<Int, String>>()
 
-  private val exceptionHandler = CoroutineExceptionHandler { ctx, _ ->
-    ctx.cancel()
+  fun getRange(path: String) {
+    viewModelScope.launch {
+      when(val result = getRanges(path)) {
+        is Result.Success -> listOfRanges.postValue(result.data!!)
+        is Result.Error -> {
+
+        }
+      }
+    }
   }
 
-  fun getRange(path: String) = viewModelScope.launch(Dispatchers.IO + exceptionHandler) {
-    listOfRanges.postValue(useCaseLeaderboards.getRanges.execute(path))
-  }
+  fun getMaps(type: Int): MutableMap<Int, String> {
+    var map = mutableMapOf<Int, String>()
+    viewModelScope.launch {
+      when(val result = getMapsUseCase(type)) {
+        is Result.Success -> map = result.data
+        is Result.Error -> {
 
-  fun getMaps(type: Int) = viewModelScope.launch(Dispatchers.IO + exceptionHandler) {
-    useCaseLeaderboards.getMaps.execute(type)
+        }
+      }
+    }
+    return map
   }
 }
