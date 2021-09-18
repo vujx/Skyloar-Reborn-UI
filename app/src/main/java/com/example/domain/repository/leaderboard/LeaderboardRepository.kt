2c80d615632a17a73bcacf4c7382a507f63a7ee1
tv <tomislav.vujevic@undabot.com>
2021-09-18 21:40:45 +0200

    Refactor usecases for maps-months-difficulties

--- app/src/main/java/com/example/domain/repository/leaderboard/LeaderboardRepository.kt
+++ app/src/main/java/com/example/domain/repository/leaderboard/LeaderboardRepository.kt
@@ -1,8 +1,10 @@
 package com.example.domain.repository.leaderboard
 
+import com.example.util.Result
+
 class LeaderboardRepository(private val leaderboardsData: LeaderboardsNetworkDataSource) {
 
-  suspend fun getRange(path: String) = leaderboardsData.getRange(path)
+  suspend fun getRange(path: String): Result<MutableMap<Int, String>> = leaderboardsData.getRange(path)
 
   suspend fun getMaps(type: Int) = leaderboardsData.getMaps(type)
 }
