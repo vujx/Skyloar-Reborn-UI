fd2f4cdfd3fb258340dbfbfb62a504f2036ffda8
tv <tomislav.vujevic@undabot.com>
2021-09-19 11:23:44 +0200

    Add base view model which calculate search results

---
+++ app/src/main/java/com/example/presentation/ui/BaseViewModel.kt
@@ -0,0 +1,36 @@
+package com.example.presentation.ui
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.data.model.auction.NumberOfSearchResultsEntity
+import com.example.util.Result
+import kotlinx.coroutines.launch
+
+open class BaseViewModel : ViewModel() {
+
+  val numOfSearchResult = MutableLiveData<Int>()
+  val pageResult = MutableLiveData<String>()
+
+  fun getNumOfSearchResult(
+    result: Result<NumberOfSearchResultsEntity>,
+    page: Int
+  ) = viewModelScope.launch {
+    when (result) {
+      is Result.Success -> {
+        val countSearch = result.data.count
+        numOfSearchResult.postValue(countSearch)
+        val numOfPage = (countSearch.toDouble() / 20)
+        when {
+          countSearch == 0 || (numOfPage < 1 && countSearch != 0) -> pageResult.postValue("1 / 1")
+          (countSearch.toDouble() / numOfPage) % 1 == 0.0 -> pageResult.postValue("$page / ${((countSearch / 20))}")
+          else -> pageResult.postValue("$page / ${((countSearch / 20) + 1)}")
+        }
+      }
+      is Result.Error -> {
+        numOfSearchResult.postValue(0)
+        pageResult.postValue("1 / 1")
+      }
+    }
+  }
+}
--- app/src/main/java/com/example/presentation/ui/leaderboards/viewmodel/PvEPlayerViewModel.kt
+++ app/src/main/java/com/example/presentation/ui/leaderboards/viewmodel/PvEPlayerViewModel.kt
@@ -2,29 +2,32 @@ package com.example.presentation.ui.leaderboards.viewmodel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.domain.model.PvEPlayer
 import com.example.domain.usecase.leaderboards.pve.GetNumOfPvESearchResult
-import com.example.domain.usecase.leaderboards.pvp.GetPvPPlayers
+import com.example.domain.usecase.leaderboards.pve.GetPvEPlayers
+import com.example.presentation.ui.BaseViewModel
 import com.example.util.Resource
 import kotlinx.coroutines.launch
 import com.example.util.Result
 
 class PvEPlayerViewModel(
-  private val getPvPPlayersUseCase: GetPvPPlayers,
+  private val getPvEPlayersUseCase: GetPvEPlayers,
   private val getNumOfPvESearchResult: GetNumOfPvESearchResult
-) :
-  ViewModel() {
+) : BaseViewModel() {
 
   private val _pvePlayer = MutableLiveData<Resource<List<PvEPlayer>?>>()
   val pvePlayer: LiveData<Resource<List<PvEPlayer>?>> = _pvePlayer
 
-  val numOfSearchResult = MutableLiveData<Int>()
-  val pageResult = MutableLiveData<String>()
-
   init {
-    getPvEPlayers(1, 1, 9, 0, 1, 20)
+    getPvEPlayers(
+      1,
+      1,
+      9,
+      0,
+      1,
+      20
+    )
   }
 
   fun getPvEPlayers(
@@ -37,7 +40,7 @@ class PvEPlayerViewModel(
   ) {
     viewModelScope.launch {
       _pvePlayer.postValue(Resource.Loading())
-      when (val result = getPvPPlayersUseCase(listOf(type, players, map, month, page, number))) {
+      when (val result = getPvEPlayersUseCase(listOf(type, players, map, month, page, number))) {
         is Result.Success -> {
           result.data?.let {
             if (it.isEmpty()) _pvePlayer.postValue(Resource.Empty())
@@ -46,37 +49,15 @@ class PvEPlayerViewModel(
         is Result.Error -> {
         }
       }
-    }
-    getNumOfPvPSearchResult(type, players, map, month, page)
-  }
-
-  private fun getNumOfPvPSearchResult(
-    type: Int,
-    players: Int,
-    map: Int,
-    month: Int,
-    page: Int
-  ) = viewModelScope.launch {
-    when (
-      val result = getNumOfPvESearchResult(
-        type,
-        players,
-        map,
-        month
+      getNumOfSearchResult(
+        getNumOfPvESearchResult(
+          type,
+          players,
+          map,
+          month
+        ),
+        page
       )
-    ) {
-      is Result.Success -> {
-        val countSearch = result.data.count
-        val numOfPage = (countSearch.toDouble() / 20)
-        numOfSearchResult.postValue(countSearch)
-        when {
-          countSearch == 0 -> pageResult.postValue("1 / 1")
-          (countSearch.toDouble() / numOfPage) % 1 == 0.0 -> pageResult.postValue("$page / ${((countSearch / numOfPage))}")
-          else -> pageResult.postValue("$page / ${((countSearch / numOfPage) + 1)}")
-        }
-      }
-      is Result.Error -> {
-      }
     }
   }
 }
