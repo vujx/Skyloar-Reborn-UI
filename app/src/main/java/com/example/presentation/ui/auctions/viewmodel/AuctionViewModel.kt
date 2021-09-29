c1f893b537af1dd656d13671433320489be37d87
tv <tomislav.vujevic@undabot.com>
2021-09-29 23:33:42 +0200

    Fetching data move to usecase

--- app/src/main/java/com/example/presentation/ui/auctions/viewmodel/AuctionViewModel.kt
+++ app/src/main/java/com/example/presentation/ui/auctions/viewmodel/AuctionViewModel.kt
@@ -4,20 +4,15 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.data.model.auction.AuctionEntityItem
-import com.example.data.model.auction.NumberOfSearchResultsEntity
 import com.example.domain.usecase.auction.GetListOfAuctions
-import com.example.domain.usecase.auction.GetNumberOfSearchResults
 import com.example.presentation.ui.BaseViewModel
 import com.example.presentation.ui.HandleError
 import com.example.util.Resource
 import com.example.util.Result
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.launch
 
 class AuctionViewModel(
   private val getAuctions: GetListOfAuctions,
-  private val getNumberOfSearchResults: GetNumberOfSearchResults,
   private val handleError: HandleError,
 ) : BaseViewModel() {
 
