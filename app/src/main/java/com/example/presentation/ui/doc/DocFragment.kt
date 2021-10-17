a9040f5593bb5ccfe7e4d37c4e5acb592c12bd85
tomislavvujevic <tomislav.vujevic@undabot.com>
2021-10-17 13:19:05 +0200

    Add click listener on cards

--- app/src/main/java/com/example/presentation/ui/doc/DocFragment.kt
+++ app/src/main/java/com/example/presentation/ui/doc/DocFragment.kt
@@ -5,10 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.R
 import com.example.databinding.FragmentDocBinding
+import com.example.presentation.ui.BaseFragment
 import com.example.presentation.ui.doc.model.Doc
 
-class DocFragment : Fragment() {
+class DocFragment : BaseFragment(R.layout.fragment_doc) {
+
+  companion object {
+
+    const val DOC_AUCTION = "http://185.203.18.254:7750/api/docs/auctions/"
+    const val DOC_STAT = "http://185.203.18.254:7750/api/docs/stats/"
+    const val DOC_LEADERBOARDS = "http://185.203.18.254:7750/api/docs/leaderboards/"
+    const val DOC_INFO = "http://185.203.18.254:7750/api/docs/info/"
+  }
 
   private var _binding: FragmentDocBinding? = null
   private val binding get() = _binding!!
@@ -16,19 +26,34 @@ class DocFragment : Fragment() {
   override fun onCreateView(
     inflater: LayoutInflater,
     container: ViewGroup?,
-    savedInstanceState: Bundle?
+    savedInstanceState: Bundle?,
   ): View {
     _binding = FragmentDocBinding.inflate(inflater, container, false)
 
-    binding.docAuction.bind(Doc("Auction", "Docs #1", "Click here to open docs in new tab."))
-    binding.docStat.bind(Doc("Statistics", "Docs #2", "Click here to open docs in new tab."))
-    binding.docLeader.bind(Doc("Leaderboards", "Docs #3", "Click here to open docs in new tab."))
-    binding.docInfo.bind(Doc("Info", "Docs #4", "Click here to open docs in new tab."))
+    clickListeners()
     return binding.root
   }
 
   override fun onDestroy() {
     super.onDestroy()
     _binding = null
+  }
+
+  private fun clickListeners() {
+    binding.firstCard.setOnClickListener {
+      openBrowser(DOC_AUCTION)
+    }
+
+    binding.secondCard.setOnClickListener {
+      openBrowser(DOC_STAT)
+    }
+
+    binding.thirdCard.setOnClickListener {
+      openBrowser(DOC_LEADERBOARDS)
+    }
+
+    binding.fourthCard.setOnClickListener {
+      openBrowser(DOC_INFO)
+    }
   }
 }
